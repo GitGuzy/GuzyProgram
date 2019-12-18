eea46a6af143d5384e2ed9fd7989048493347696
GuzyBucket <guzy.kontakt@gmail.com>
2019-12-18 21:21:15 +0100

    First BullsCows game upload

---
+++ BullCowGame/main 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	//introduce the game
+	constexpr int WORLD_LENGHT = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game\n";
+	cout << "Can you guess the " << WORLD_LENGHT;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+
+	// get a guess from the player
+
+	
+	// repeat the guess back to them
+
+	return 0;
+}
